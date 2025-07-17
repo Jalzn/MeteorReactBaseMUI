6a94a5fba9c8a2721f6725c5ff2918de75893555
Jalzn <jalmirfsjr@gmail.com>
2025-07-17 10:02:03 -0300

    fix missing sort for home page tasks

--- imports/sysPages/pages/home/homeController.tsx
+++ imports/sysPages/pages/home/homeController.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { createContext, useMemo } from 'react';
 import { useTracker } from 'meteor/react-meteor-data';
 import { taskApi } from '/imports/modules/task/api/task.api';
 import HomeView from './homeView';
 import { ITaskWithProfile } from '/imports/modules/task/task.types';
+import AuthContext from '/imports/app/authProvider/authContext';
 
 interface IHomeContext {
 	tasks: ITaskWithProfile[];
@@ -13,17 +14,22 @@ interface IHomeContext {
 export const HomeContext = createContext<IHomeContext>({} as IHomeContext);
 
 export const HomeController = () => {
+	const { user } = useContext(AuthContext);
+
 	const { loading, tasks } = useTracker(() => {
 		const subHandle = taskApi.subscribe(
 			'taskList',
-			{},
 			{
-				sort: { createdAt: -1 },
+				'$or': [{ isPrivate: false }, { ownedBy: user?._id }]
+			},
+			{
+				sort: { createdat: -1 },
 				limit: 5
 			}
 		);
 
 		const tasks = subHandle?.ready() ? taskApi.find({}).fetch() : [];
+		console.log(tasks);
 
 		return {
 			tasks,
